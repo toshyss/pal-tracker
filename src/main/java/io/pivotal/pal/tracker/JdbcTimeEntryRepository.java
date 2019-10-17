8ac1d6e3299a7ff7f8b2a28f1082ff8fbe8cdc2a
toshyss <test@samplesample.com>
2019-10-17 14:43:12 +0900

    added jdbctemplate

---
+++ src/main/java/io/pivotal/pal/tracker/JdbcTimeEntryRepository.java
@@ -0,0 +1,90 @@
+package io.pivotal.pal.tracker;
+
+import com.mysql.cj.jdbc.MysqlDataSource;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+
+import java.sql.PreparedStatement;
+import java.util.List;
+import java.util.Map;
+
+public class JdbcTimeEntryRepository implements TimeEntryRepository {
+
+    private JdbcTemplate jdbcTemplate;
+    private KeyHolder keyHolder;
+
+    public JdbcTimeEntryRepository(MysqlDataSource dataSource) {
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
+        keyHolder = new GeneratedKeyHolder();
+    }
+
+    @Override
+    public TimeEntry create(TimeEntry timeEntry) {
+        //final String sql = "insert into time_entries(id, project_id, user_id, date, hours) values(?, ?, ?, ?, ?)";
+        final String sql = "insert into time_entries(project_id, user_id, date, hours) values(?, ?, ?, ?)";
+        //jdbcTemplate.update(sql, new Object[]{timeEntry.getId(), timeEntry.getProjectId(), timeEntry.getUserId(), timeEntry.getDate(), timeEntry.getHours()});
+        jdbcTemplate.update(connection -> {
+            PreparedStatement ps = connection.prepareStatement(sql, new String[]{"id"});
+//            ps.setLong(1, keyHolder.getKey().longValue());
+            ps.setLong(1, timeEntry.getProjectId());
+            ps.setLong(2, timeEntry.getUserId());
+            ps.setObject(3, timeEntry.getDate());
+            ps.setInt(4, timeEntry.getHours());
+            return ps;
+        }, keyHolder);
+
+        timeEntry.setId(keyHolder.getKey().longValue());
+
+        return timeEntry;
+    }
+
+    @Override
+    public TimeEntry find(long id) {
+        final String sql = "select * from time_entries where id = ?";
+        List<TimeEntry> entries = jdbcTemplate.query(
+                sql,
+                new Object[]{id},
+                new BeanPropertyRowMapper<>(TimeEntry.class)
+        );
+        //Map<String, Object> map = jdbcTemplate.queryForMap(sql, id);
+        if (entries.isEmpty()) {
+            return null;
+        }
+        return entries.get(0);
+    }
+
+    @Override
+    public List<TimeEntry> list() {
+        final String sql = "select * from time_entries";
+        List<TimeEntry> entries = jdbcTemplate.query(
+                sql,
+                new BeanPropertyRowMapper<>(TimeEntry.class)
+                );
+        if (entries.isEmpty()) {
+            return null;
+        }
+        return entries;
+    }
+
+    @Override
+    public TimeEntry update(long id, TimeEntry timeEntry) {
+        final String sql = "update time_entries set project_id = ?, user_id = ?, date = ?, hours = ? where id = ?";
+
+        int rows = jdbcTemplate.update(sql, new Object[]{timeEntry.getProjectId(), timeEntry.getUserId(), timeEntry.getDate(), timeEntry.getHours(), id});
+        if (rows == 0) {
+            return null;
+        }
+        timeEntry.setId(id);
+
+        return timeEntry;
+    }
+
+    @Override
+    public void delete(long id) {
+        final String sql = "DELETE FROM time_entries where id = ?";
+        jdbcTemplate.update(sql,
+                new Object[]{id});
+    }
+}
--- src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
+++ src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
@@ -1,8 +1,11 @@
 package io.pivotal.pal.tracker;
 
+import com.mysql.cj.jdbc.MysqlDataSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 public class PalTrackerApplication {
@@ -14,6 +17,10 @@ public class PalTrackerApplication {
 
     @Bean
     TimeEntryRepository timeEntryRepository() {
-            return new InMemoryTimeEntryRepository();
+//            return new InMemoryTimeEntryRepository();
+        MysqlDataSource dataSource = new MysqlDataSource();
+        dataSource.setUrl(System.getenv("SPRING_DATASOURCE_URL"));
+        return new JdbcTimeEntryRepository(dataSource);
     }
+
 }
